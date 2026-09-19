4251a4c881f64401eb77093f2798a062ad20991a
과제2.c <usuhuu420@gmail.com>
2026-09-19 13:25:26 +0900

    only app

--- app/book/[hall_id].tsx
+++ app/book/[hall_id].tsx
@@ -1,17 +1,12 @@
-import { useCallback, useMemo } from "react";
+import { useMemo } from "react";
 import { useLocalSearchParams } from "expo-router";
 import { View } from "react-native";
-import { EsportHallDataType, SportHallDataType } from "@/types/hall_info_type";
-import { HallTypesSeparator } from "@/types/hall_separator_type";
-import SportHall from "@/components/hall_components/halls/sport_hall";
-import CombinedEsportHall from "./esport/[hall_id]";
-import Animated, { FadeIn, FadeOut } from "react-native-reanimated";
+import { SportHallDataType } from "@/types/hall_info_type";
+import Animated from "react-native-reanimated";
 import { useTheme } from "@/context/theme_context";
 import OwnActivaterIndicator from "@/components/ui/loader_indicator";
 import { useHallInfo } from "@/context/hall_info_context";
-import BookingBoardGame from "./boardGame/[hall_id]";
 import Main from "@/components/hall_components/halls/main";
-import { SafeAreaView } from "react-native-safe-area-context";
 import { getHallTypeFromCategories } from "@/utils/duration_price";
 
 export default function DetailsPage() {
